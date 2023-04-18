e4ab97859ba3a572e4cd8ac7cd3d8d33453d9a55
Aidan Juma <aidan@aidanjuma.dev>
2023-04-18 22:31:27 +0100

    🕵️‍♂️ added search endpoint for binance!

--- src/routes/crypto/binance.ts
+++ src/routes/crypto/binance.ts
@@ -1,4 +1,6 @@
+import { IBinanceAsset } from "equity-scraper/dist/models/types";
 import Binance from "equity-scraper/dist/providers/crypto";
+import { binarySearch } from "../../utils/search";
 import {
   FastifyRequest,
   FastifyReply,
@@ -12,17 +14,60 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
   fastify.get("/", (_, rp) => {
     rp.status(200).send({
       intro: `Welcome to the Binance provider for the Equity API! Check out the provider's own website at ${binance.toString.baseUrl} The available routes are listed below.`,
-      routes: ["/:query", "/:ticker"],
+      routes: ["/assets", "/search/:query", "/asset/:ticker"],
     });
   });
 
   fastify.get(
-    "/:query",
-    async (request: FastifyRequest, reply: FastifyReply) => {}
+    "/assets",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      reply.status(200).send(await binance.getAvailableAssets());
+    }
   );
 
   fastify.get(
-    "/:ticker",
+    "/search/:query",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const query: string = decodeURIComponent(
+        (request.params as { query: string }).query
+      ).toUpperCase();
+
+      const assets: IBinanceAsset[] = await binance.getAvailableAssets();
+
+      /*
+        Insertion Sort:
+          Worst/Average-Case: O(n^2)
+          Best-Case: O(n)
+      */
+      const sortedArray: IBinanceAsset[] = assets.sort((a, b) =>
+        a.ticker > b.ticker ? 1 : b.ticker > a.ticker ? -1 : 0
+      );
+
+      /*
+        Binary Search:
+          Worst/Average-Case: O(log n)
+          Best-Case: O(1)
+      */
+      const matches: number[] = binarySearch(
+        sortedArray.map((asset) => asset.ticker),
+        query
+      );
+
+      if (matches.length <= 0) {
+        reply
+          .status(404)
+          .send(`404: No results returned for query: "${query}"`);
+        return;
+      }
+
+      const results: IBinanceAsset[] = matches.map((i) => assets[i]);
+
+      reply.status(200).send({ results: results });
+    }
+  );
+
+  fastify.get(
+    "/asset/:ticker",
     async (request: FastifyRequest, reply: FastifyReply) => {}
   );
 };
--- src/routes/crypto/index.ts
+++ src/routes/crypto/index.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance, RegisterOptions } from "fastify";
 
+import binance from "./binance";
 const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
+  await fastify.register(binance, { prefix: "/binance" });
+
   fastify.get("/", (_, rp) => {
     rp.status(200).send({
       intro:
